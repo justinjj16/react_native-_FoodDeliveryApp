b28e62506d6ec238691aec53b8927ce72989cfcd
justin <justin.j@neoito.com>
2021-04-04 15:36:39 +0530

    react_native _FoodDeliveryApp

---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyCBkwTKE7O5HRJ4rrPVzTwORiruFzepm68"
+
+export default GOOGLE_API_KEY
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import Restaurant from './Restaurant';
+import OrderDeliverys from './OrderDeliverys';
+
+export{
+    Home,
+    Restaurant,
+    OrderDeliverys
+}
